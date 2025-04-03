8a0ef47b02ef10dd3c3e6a7d96dd46e3f20b7e2d
Bartomiej Stec <itscriptcoding@gmail.com>
2025-04-03 19:50:28 +0200

    edit: startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 apt-get update && apt-get install -y librdkafka-dev
-pecl install rdkafka
+yes '' | pecl install rdkafka
 echo "extension=rdkafka.so" >> /home/site/wwwroot/.user.ini
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default && service nginx reload
